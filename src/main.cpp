1d7318d762409d7ad7e91e5100910ab5c3fc540a
mi nombre <myemail@example.com>
2021-12-21 13:27:32 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero1;
+    int numero2;
+    int Suma;
+
+    cout << "Escriba el primer numero";
+    cin>>numero1;
+    
+    cout << "Escriba el segundo numero";
+    cin>>numero2;
+
+    Suma=numero1+numero2;
+
+    cout<<"El resultado de la suma es:"<<Suma<<endl;
+
+    return 0;
+}
